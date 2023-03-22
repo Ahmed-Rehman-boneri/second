99be66eb81562c2b94454d7cea307c0fcdd7bb65
Ahmed rehman <ahmedrehman.ar7@gmail.com>
2023-03-22 17:58:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// javascript log
+// alert("please subscrib");
+// document.write("this is java")
+
+//  javascript console
+/*
+console.log("hello world");
+console.warn("this is warnning")
+console.error("this is error")
+*/
+/*
+var number1=(34);
+var number2=(66);
+
+console.log(number1+number2);
+ 
+
+
+var string1=('this is a string')
+var string2=("this is also string")
+
+
+var nub1=(34);
+var nub2=(46)
+
+var marks={
+    ahmed:34,
+    rehman:45,
+    khan:54,
+
+}
+console.log(marks)
+*/
+function avg (a, b){
+    c = (a + b)/2; 
+    return  c;
+
+
+}
+
+ c1 = avg(12, 8);   
+ c2 = avg(8, 2);
+
+//  console.log(c1, c2);
+
+var age = 8
+if(age >2){
+    console.log('you are a man')
+
+}
+else{
+    console.log('you are a kid')
+}
+
+var arr = [1,2,3,4,5,6,7];
+console.log(arr);
+for(var i = 0;i<arr.length;i++){
+    console.log(arr[i])
+}
